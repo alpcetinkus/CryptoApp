f8bbf6d3a7740eee7633421197f3e204270e1168
alp cetinkus <alpcetinkus@gmail.com>
2023-03-26 18:23:22 +0300

    Revert "change structure"
    This reverts commit f33348f636cf7b333ad1b018bca7c3fdd1fb2fd2.

--- app/src/main/java/com/example/coinsapp/MainActivity.kt
+++ app/src/main/java/com/example/coinsapp/MainActivity.kt
@@ -2,9 +2,7 @@ package com.example.coinsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.PopupMenu
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,12 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        val navController = navHostFragment!!.findNavController()
-
-        val popupMenu = PopupMenu(this,null)
-        popupMenu.inflate(R.menu.bottom_bar_menu)
-        bottomBar.setupWithNavController(popupMenu.menu, navController)
-
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(bottomNav,navHostFragment.navController)
     }
 }
--- app/src/main/java/com/example/coinsapp/model/CryptoCurrency.kt
+++ app/src/main/java/com/example/coinsapp/model/CryptoCurrency.kt
@@ -1,7 +1,5 @@
 package com.example.coinsapp.model
 
-import java.io.Serializable
-
 data class CryptoCurrency(
     val auditInfoList: List<AuditInfo>,
     val circulatingSupply: Double,
@@ -21,16 +19,4 @@ data class CryptoCurrency(
     val symbol: String,
     val tags: List<String>,
     val totalSupply: Double
-) : Serializable {
-    override fun equals(other: Any?): Boolean {
-        return super.equals(other)
-    }
-
-    override fun hashCode(): Int {
-        return super.hashCode()
-    }
-
-    override fun toString(): String {
-        return super.toString()
-    }
-}
+)
